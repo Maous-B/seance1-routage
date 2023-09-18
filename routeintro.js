a0c6a2520de03ebf519f031eca33be29d6a48d91
Maous-B <maousgaming01@gmail.com>
2023-09-18 12:18:34 +0200

    Update

--- routeintro.js
+++ routeintro.js
@@ -1,21 +1,62 @@
+/*import {createRequire} from "module"
+const require = createRequire(import.meta.url)
+import {listeEleves} from "./sources/eleves.mjs";*/
+
 const express = require('express')
 const path = require('path')
 const app = express()
 
+app.get('/', (requete, resultat) =>
+
+{
+    resultat.send("Bonjour");
+})
+
+app.get('/accueil', (requete, resultat) =>
+
+{
+    resultat.send("Bienvenue sur la page d'accueil");
+})
+
+app.get('/panorama', (requete, resultat) =>
+{
+    resultat.sendFile(path.join(__dirname, '/plandusite.html'));
+})
+
+app.get('/bienvenue', (requete, resultat) =>
+{
+    resultat.sendFile(path.join(__dirname, '/bienvenue.html'));
+})
 
 
-app.get('/', (requete, resultat) => {
-    //resultat.send('Bonjour aux sio2 slam')
-    const options = {
-        root : path.join(__dirname, 'public'),
-        dotfiles : 'deny',
-        headers : {
-            'x-timestamp': Date.now(),
-            'x-sent': true
-        }
-    }
 
-    const fileName = 'bienvenue.html'
+
+app.get('/exempleParams', (requete, resultat, next) => {
+
+    const nomEtudiant = requete.query.nomEtudiant
+    const prenomEtudiant = requete.query.prenomEtudiant
+    resultat.send('Bonjour mr ou mme ' + nomEtudiant + " " + prenomEtudiant)
+})
+
+app.get('/exempleParams/:idEleve', (requete, resultat, next) => {
+    const nomE = requete.params.idEleve
+    resultat.send('Bonjour mr  ' + nomE )
+})
+
+app.get('/exempleParams/valeurs/:value', (requete, resultat, next) => {
+
+
+    valeur = requete.params.value
+
+    if(isNaN(valeur))
+
+        resultat.send("Le paramètre est éronné")
+
+    else
+
+        resultat.send("La valeur est : " + valeur)
+
+
 })
 
 
---
+++ sources/eleves.mjs
@@ -0,0 +1,6 @@
+const listeEleves = [
+    {nom : 'Roy', prenom : 'Abinash'},
+    {nom : 'Zabolo', prenom : 'Marie'},
+]
+
+export {listeEleves}
